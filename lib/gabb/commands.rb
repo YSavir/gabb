83879dbee678e970b45769235acdac3632abb5d2
Yaniv Savir <saviry@gmail.com>
2014-10-26 19:26:39 -0400

    line break and beat console commands; module Commands -> ConsoleCommands

--- lib/gabb/commands.rb
+++ lib/gabb/commands.rb
@@ -1,10 +1,18 @@
 module GABB
 
-  module Commands
+  module ConsoleCommands
 
     def wait(command=nil)
       puts command || "Press Enter to proceed."
       gets.chomp
+    end
+
+    def line_break
+      puts "===================================="
+    end
+
+    def beat
+      puts ""
     end
 
   end
