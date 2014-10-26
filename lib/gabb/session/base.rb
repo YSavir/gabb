cce855813a624eb442f38c8e1fe4eb62a5a3616f
Yaniv Savir <saviry@gmail.com>
2014-10-26 19:30:40 -0400

    session divided into session::base and session::manager

---
+++ lib/gabb/session/base.rb
@@ -0,0 +1,21 @@
+module GABB
+
+  module Session
+
+    class Base
+      attr_reader :name
+
+      def initialize
+        @exercises_manager = GABB::ExercisesManager.new
+        start_exercise
+      end
+
+      private
+
+      def start_exercise
+        exercise = @exercises_manager.choose_exercise
+        exercise.new(self)
+      end
+    end
+  end
+end
