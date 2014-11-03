88815ffd8693697dc5d1ea8f97232ab9cb360d8e
Yaniv Savir <saviry@gmail.com>
2014-11-02 19:53:24 -0500

    detail validator dialogues are instance variables

--- lib/gabb/detail_validator.rb
+++ lib/gabb/detail_validator.rb
@@ -1,10 +1,15 @@
 module GABB
 
   class DetailValidator
+    attr_writer :guess_file_prompt, :guess_line_prompt, :incorrect_file, :incorrect_line
 
     def initialize(exercise)
       @exercise = exercise
       @details_are_validated = false
+      @guess_file_prompt =  "In what file is the error?".blue
+      @guess_line_prompt = "On what line is the error?".blue
+      @incorrect_file = "That's not the right file.".yellow 
+      @incorrect_line = "That's not the right line.".yellow
     end
 
     def find_details(error)
@@ -21,17 +26,17 @@ module GABB
     end
 
     def guess_file
-      puts "In what file is the error?".blue
+      puts @guess_file_prompt
       unless gets.chomp == @error_file
-        puts("That's not the right file.".yellow)
+        puts @incorrect_file
         guess_file
       end
     end
 
     def guess_line
-      puts "On what line is the error?".blue
+      puts @guess_line_prompt
       unless gets.chomp == @error_line
-        puts("That's not the right line.".yellow)
+        puts @incorrect_line
         guess_line
       end
     end
