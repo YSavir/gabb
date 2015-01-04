d2f606f8970ac0e910dd9cfc367139b928f13784
Yaniv Savir <saviry@gmail.com>
2015-01-04 18:52:03 -0500

    adds tests for directory mapper

--- spec/gabb/directory_mapper_spec.rb
+++ spec/gabb/directory_mapper_spec.rb
@@ -1,4 +1,3 @@
-require_relative '../spec_helper'
 require_relative '../../lib/gabb/directory_mapper'
 
 describe GABB::DirectoryMapper do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,2 @@
 require 'rspec'
+require 'active_support/core_ext/kernel/reporting'
